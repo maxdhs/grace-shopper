2e4a57e2452c9e489ed3f8b07724331e522612de
Lexi Miller <lexim515@gmail.com>
2022-04-05 21:26:03 -0400

    creating some tables

--- db/products.js
+++ db/products.js
@@ -1,5 +1,26 @@
 const client = require("./");
 
+const createProducts = async ({
+  title,
+  description,
+  price,
+  quantity,
+  category,
+}) => {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `INSERT INTO products(title, description, price, quantity, category) VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
+      [title, description, price, quantity, category]
+    );
+
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
+
 const getProducts = async () => {
   const response = await client.query(`
     SELECT * FROM products;
--- db/seed.js
+++ db/seed.js
@@ -1,16 +1,77 @@
 require("dotenv").config();
 const client = require("./");
 
-const seedDB = async () => {
+const dropTables = async () => {
   await client.query(`
-    DROP TABLE IF EXISTS products;
+  DROP TABLE IF EXISTS products;
+  DROP TABLE IF EXISTS users;
+  DROP TABLE IF EXISTS orders;
+  `);
+};
 
-    CREATE TABLE products (id SERIAL PRIMARY KEY, title VARCHAR(255) UNIQUE);
-
-    INSERT INTO products (title) VALUES ('Acer Nitro 5 Laptop');
+const createTables = async () => {
+  await client.query(`
+    CREATE TABLE products (id SERIAL PRIMARY KEY, title VARCHAR(255) UNIQUE NOT NULL, description TEXT NOT NULL, price integer NOT NULL, quantity integer NOT NULL, category TEXT NOT NULL);
     `);
 
   console.log("DB SEEDED.");
 };
 
-seedDB();
+const rebuildDB = async () => {
+  try {
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.log("Error during rebuildDB");
+    throw error;
+  }
+};
+
+const createInitialProducts = async () => {
+  try {
+    const productsToCreate = [
+      {
+        title: "shoe1",
+        description: "this is a shoe",
+        price: 10,
+        quantity: 100,
+        category: "sneakers",
+      },
+      {
+        title: "shoe2",
+        description: "this is a shoe",
+        price: 20,
+        quantity: 100,
+        category: "heels",
+      },
+      {
+        title: "shoe3",
+        description: "this is a shoe",
+        price: 30,
+        quantity: 100,
+        category: "flats",
+      },
+      {
+        title: "shoe4",
+        description: "this is a shoe",
+        price: 40,
+        quantity: 100,
+        category: "sneakers",
+      },
+      {
+        title: "shoe5",
+        description: "this is a shoe",
+        price: 50,
+        quantity: 100,
+        category: "sandals",
+      },
+    ];
+    const products = await Promise.all(productsToCreate.map(createProducts));
+  } catch (error) {
+    throw error;
+  }
+};
+
+rebuildDB()
+  .catch(console.error)
+  .finally(() => client.end());
