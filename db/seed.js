97dcb5d1970f3564b64e3f4fb5d100debee7cfad
JacobL-FTB <jlarson2002@icloud.com>
2022-04-07 18:54:24 -0400

    Seed Update

--- db/seed.js
+++ db/seed.js
@@ -1,16 +1,39 @@
-require("dotenv").config();
-const client = require("./");
+require('dotenv').config();
+const client = require('./');
 
 const seedDB = async () => {
   await client.query(`
+    DROP TABLE IF EXISTS carts_products;
     DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS carts;
+    DROP TABLE IF EXISTS reviews;
+    DROP TABLE IF EXISTS users;
 
-    CREATE TABLE products (id SERIAL PRIMARY KEY, title VARCHAR(255) UNIQUE);
+    CREATE TABLE products (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255) UNIQUE,
+      description TEXT NOT NULL,
+      price INTEGER NOT NULL,
+      quantity INTEGER NOT NULL,
+      catagory VARCHAR(255) NOT NULL );
 
-    INSERT INTO products (title) VALUES ('Acer Nitro 5 Laptop');
-    `);
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) UNIQUE NOT NULL);
 
-  console.log("DB SEEDED.");
+    CREATE TABLE orders (id SERIAL PRIMARY KEY, "userId" INTEGER REFERENCES users(id), price INTEGER NOT NULL);
+    CREATE TABLE reviews (id SERIAL PRIMARY KEY, "userId" INTEGER REFERENCES users(id), text TEXT NOT NULL);
+
+    CREATE TABLE orders_products (
+      id SERIAL PRIMARY KEY,
+      count INTEGER NOT NULL,
+      "orderId" INTEGER REFERENCES orders(id),
+      "productId" INTEGER REFERENCES products(id));
+  `);
+
+  console.log('DB SEEDED.');
 };
 
 seedDB();
