15c401eb1c2ee84eb67c5b2bda1b5ae30e948f66
Lexi Miller <lexim515@gmail.com>
2022-04-12 20:40:22 -0400

    stashing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const App = () => {
-  // const [products, setProducts] = useState([]);
-
-  // const fetchProducts = async () => {
-  //   const response = await fetch("/api/products");
-  //   const info = await response.json();
-  //   setProducts(info);
-  // };
-
-  // useEffect(() => {
-  //   fetchProducts();
-  // }, []);
-
-  return <>Full-Stack App!!!!</>;
+  return <>This is a fullstack app!!!</>;
 };
 
 export default App;
