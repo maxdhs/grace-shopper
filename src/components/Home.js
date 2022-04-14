02f8e5f5825f3d2f176043abbd0bd3e6a5743576
David Lu <davidlu@davids-mbp.attlocal.net>
2022-04-14 19:47:02 -0400

    home design

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="main-head">Welcome to Sick Kicks</h1>
+     <body>
+         <section>
+             <div className="row">
+                 <div>
+                 <FontAwesomeIcon icon="fa-light fa-shoe-prints" />
+                 <FontAwesomeIcon icon="fa-solid fa-shoe-prints" />
+                 <FontAwesomeIcon icon="fa-light fa-shoe-prints" />
+                 <FontAwesomeIcon icon="fa-thin fa-shoe-prints" />
+                 <FontAwesomeIcon icon="fa-duotone fa-shoe-prints" />
+                 <FontAwesomeIcon icon="fa-duotone fa-boot" />
+                 <FontAwesomeIcon icon="fa-regular fa-boot" />
+                 <FontAwesomeIcon icon="fa-light fa-boot" />
+                 <FontAwesomeIcon icon="fa-thin fa-boot" />
+                 <FontAwesomeIcon icon="fa-duotone fa-boot" />
+                 <FontAwesomeIcon icon="fa-duotone fa-boot-heeled" />
+                 <FontAwesomeIcon icon="fa-regular fa-boot-heeled" />
+                 <FontAwesomeIcon icon="fa-light fa-boot-heeled" />
+                 <FontAwesomeIcon icon="fa-thin fa-boot-heeled" />
+                 <FontAwesomeIcon icon="fa-duotone fa-boot-heeled" />
+
+                 </div>
+             </div>
+         </section>
+     </body>
+    </>
+  );
+};
+
+export default Home;
