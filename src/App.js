76daa1517eb0ea8e5862064d5057bffda220f2f1
lisascalf <lisascalf143@gmail.com>
2022-04-14 18:37:05 -0400

    working on routes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,24 @@ const App = () => {
   //   fetchProducts();
   // }, []);
 
-  return <>Full-Stack App!!!!</>;
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login_register" element={<Login_Register />} />
+        <Route exact path="/allshoes" element={<AllShoes />} />
+        <Route exact path="/boots" element={<Boots />} />
+        <Route exact path="/heels" element={<Heels />} />
+        <Route exact path="/sandals" element={<Sandals />} />
+        <Route exact path="/sneakers" element={<Sneakers />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/admin" element={<Admin />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
