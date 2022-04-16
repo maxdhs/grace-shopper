9085ec74171f670e548eff99dd249b2db299a302
David Lu <davidlu@davids-mbp.attlocal.net>
2022-04-16 09:40:21 -0400

    Home animation final

--- src/components/Home.js
+++ src/components/Home.js
@@ -138,6 +138,101 @@ const Home = () => {
                 <img className="i" src="sneakers-4.png" width="64px" />
               </div>
             </div>
+            <div className="row2">
+              <div className="a1">
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="sneakers.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+              </div>
+            </div>
+            <div className="row1">
+              <div className="a1">
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="sneakers.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+              </div>
+            </div>
+            <div className="row2">
+              <div className="a1">
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="sneakers.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+              </div>
+            </div>
+            <div className="row1">
+              <div className="a1">
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="sneakers.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+              </div>
+            </div>
+            <div className="row2">
+              <div className="a1">
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="sneakers.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="high-heels.png" width="64" />
+                <img className="i" src="sneakers-5.png" width="64px" />
+                <img className="i" src="shoes.png" width="64px" />
+                <img className="i" src="sneakers-2.png" width="64px" />
+                <img className="i" src="sneakers-3.png" width="64px" />
+                <img className="i" src="sport-shoe.png" width="64px" />
+                <img className="i" src="sneakers-4.png" width="64px" />
+              </div>
+            </div>
           </section>
         </body>
       </main>
