019d28c6a3f68c5cc097672749b66a8ae231cd79
Emma Ruth Nagaño <emmaruthnagano@Emmas-MacBook-Pro.local>
2022-04-19 01:54:11 -0400

    pulling some files

--- src/components/Cart.js
+++
@@ -1,9 +0,0 @@
-
-
-const Cart = () => {
-    return <div>
-        <h1>hi:cart</h1>
-    </div>
-}
-
-export default Cart;
--- src/components/Register.js
+++
@@ -1,9 +0,0 @@
-
-
-const Register = () => {
-    return <div>
-        <h1>Hi: register</h1>
-    </div>
-}
-
-export default Register;
